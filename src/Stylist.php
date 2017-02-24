0b250d4cee21475683cc560571fe6cd52eb0c7fb
Ash Laidlaw <ashlaidlaw@Ashs-MacBook-Pro.local>
2017-02-24 09:10:11 -0800

    Add save and delete all tests, pass both tests

--- src/Stylist.php
+++ src/Stylist.php
@@ -1,14 +1,12 @@
 <?php
     class Stylist
     {
-        private $id;
         private $first_name;
         private $last_name;
         private $phone_number;
 
-        function __construct($id = null, $first_name, $last_name, $phone_number)
+        function __construct($first_name, $last_name, $phone_number)
         {
-            $this->id = $id;
             $this->first_name = $first_name;
             $this->last_name = $last_name;
             $this->phone_number = $phone_number;
@@ -47,6 +45,32 @@
         function getPhoneNumber()
         {
             return $this->phone_number;
+        }
+
+        function saveStylist()
+        {
+            $GLOBALS["DB"]->exec("INSERT INTO stylists (first_name, last_name, phone_number) VALUES ('{$this->getFirstName()}', '{$this->getLastName()}', {$this->getPhoneNumber()});");
+        }
+
+        static function getAllStylists()
+        {
+            $returned_stylists = $GLOBALS["DB"]->query("SELECT * FROM stylists;");
+            $stylists = array();
+
+            foreach($returned_stylists as $stylist) {
+                $first_name = $stylist["first_name"];
+                $last_name = $stylist["last_name"];
+                $phone_number = $stylist["phone_number"];
+                $test_stylist = new Stylist($first_name, $last_name, $phone_number);
+                array_push($stylists, $test_stylist);
+            }
+
+            return $stylists;
+        }
+
+        static function deleteAllStylists()
+        {
+            $GLOBALS["DB"]->exec("DELETE FROM stylists;");
         }
     }
 ?>
---
+++ tests/StylistTest.php
@@ -0,0 +1,82 @@
+<?php
+        /**
+        * @backupGlobals disabled
+        * @backupStaticAttributes disabled
+        */
+
+        require_once "src/Stylist.php";
+
+        $server = "mysql:host=localhost:8889;dbname=hair_salon_test";
+        $username = "root";
+        $password = "root";
+        $DB = new PDO($server, $username, $password);
+
+        class StylistTest extends PHPUnit_Framework_TestCase
+        {
+            protected function tearDown()
+            {
+                Stylist::deleteAllStylists();
+            }
+
+            function test_saveStylist()
+            {
+                // Arrange
+                $first_name = "John";
+                $last_name = "Smith";
+                $phone_number = 1234567890;
+                $test_stylist = new Stylist($first_name, $last_name, $phone_number);
+
+                // Act
+                $test_stylist->saveStylist();
+
+                // Assert
+                $result = Stylist::getAllStylists();
+                $this->assertEquals($test_stylist, $result[0]);
+            }
+
+            function test_getAllStylists()
+            {
+                // Arrange
+                $first_name = "John";
+                $last_name = "Smith";
+                $phone_number = 1234567890;
+                $test_stylist = new Stylist($first_name, $last_name, $phone_number);
+                $test_stylist->saveStylist();
+
+                $first_name2 = "Lucy";
+                $last_name2 = "Jones";
+                $phone_number2 = 1234567890;
+                $test_stylist2 = new Stylist($first_name2, $last_name2, $phone_number2);
+                $test_stylist2->saveStylist();
+
+                // Act
+                $result = Stylist::getAllStylists();
+
+                // Assert
+                $this->assertEquals([$test_stylist, $test_stylist2], $result);
+            }
+
+            function test_deleteAllStylists()
+            {
+                //Arrange
+                $first_name = "John";
+                $last_name = "Smith";
+                $phone_number = 1234567890;
+                $test_stylist = new Stylist($first_name, $last_name, $phone_number);
+                $test_stylist->saveStylist();
+
+                $first_name2 = "Lucy";
+                $last_name2 = "Jones";
+                $phone_number2 = 1234567890;
+                $test_stylist2 = new Stylist($first_name2, $last_name2, $phone_number2);
+                $test_stylist2->saveStylist();
+
+                //Act
+                Stylist::deleteAllStylists();
+
+                //Assert
+                $result = Stylist::getAllStylists();
+                $this->assertEquals([], $result);
+            }
+        }
+?>
