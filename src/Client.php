6ac16c04a5e1964835599ba092ddfe5aa1d66312
Ash Laidlaw <ashlaidlaw@Ashs-MacBook-Pro.local>
2017-02-24 11:39:31 -0800

    Add delete all clients method and pass test

--- src/Client.php
+++ src/Client.php
@@ -71,5 +71,10 @@
 
             return $clients;
         }
+
+        static function deleteAll()
+        {
+            $GLOBALS["DB"]->exec("DELETE FROM clients;");
+        }
     }
 ?>
--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -28,6 +28,29 @@
                 $result = Client::getAll();
                 $this->assertEquals($test_client, $result[0]);
             }
+
+            function test_deleteAllClients()
+            {
+                //Arrange
+                $first_name = "John";
+                $last_name = "Smith";
+                $phone_number = 1234567890;
+                $test_stylist = new Client($id, $first_name, $last_name, $phone_number);
+                $test_stylist->save();
+
+                $first_name2 = "Lucy";
+                $last_name2 = "Jones";
+                $phone_number2 = 1234567890;
+                $test_stylist2 = new Client($id, $first_name2, $last_name2, $phone_number2);
+                $test_stylist2->save();
+
+                //Act
+                Client::deleteAll();
+
+                //Assert
+                $result = Client::getAll();
+                $this->assertEquals([], $result);
+            }
         }
 
 ?>
